ba1fab41e1b384f9f71ef0f78dcbeb97526473e7
nurainirazali <nuraini.razali13@gamil.com>
2022-01-26 21:37:11 +0800

    edit path

--- src/main/java/com/example/cbos/UserRegisServlet.java
+++ src/main/java/com/example/cbos/UserRegisServlet.java
@@ -47,7 +47,7 @@ public class UserRegisServlet extends HttpServlet {
 
             if(row>0){
                 out.println("Record inserted");
-                RequestDispatcher dispatcher = request.getRequestDispatcher("indexStaff.jsp");
+                RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
                 dispatcher.forward (request, response);
             }else{
                 out.println("Record failed");
